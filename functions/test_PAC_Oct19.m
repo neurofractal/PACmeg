f8de4b81652a8970199378d641f966af147c3dc8
Robert Seymour <robbyseymour@gmail.com>
2019-10-04 13:27:49 +1000

    Make test code pretty

--- functions/test_PAC_Oct19.m
+++ functions/test_PAC_Oct19.m
@@ -3,18 +3,18 @@ load('test123.mat');
 
 figure; plot(signal(1:2200))
 
-cfg = [];
-cfg.Fs            = 1000;
-cfg.phase_freqs   = [2:1:10];
-cfg.amp_freqs     = [30:2:100];
-cfg.method        = 'tort';
-cfg.filt_order    = 3;
-cfg.surr_method = 'swap_blocks';
-cfg.surr_N      = 200;
-cfg.amp_bandw_method = 'number';
-cfg.amp_bandw     = 16;
+cfg                     = [];
+cfg.Fs                  = 1000;
+cfg.phase_freqs         = [2:1:10];
+cfg.amp_freqs           = [30:2:100];
+cfg.method              = 'tort';
+cfg.filt_order          = 3;
+cfg.surr_method         = 'swap_blocks';
+cfg.surr_N              = 200;
+cfg.amp_bandw_method    = 'number';
+cfg.amp_bandw           = 16;
 
-[fff,ggg] = PACmeg(cfg,signal);
+[MI_raw,MI_surr]        = PACmeg(cfg,signal);
 
 ddd = (fff - squeeze(mean(ggg,1)))./((squeeze(mean(ggg,1)))).*100;
 
